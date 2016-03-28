bc9bf21232e092944233588bc76eb9ada1d13aa8
Konstantin Kolomeetz <kolomeetz@gmail.com>
2016-03-28 21:53:37 +0300

    Add files list

--- app.rb
+++ app.rb
@@ -1,18 +1,26 @@
 require 'rubygems'
 require 'sinatra'
 
+before do
+  @upload_dir_name = 'uploads'
+end
+
 post '/upload' do
-  upload_dir_name = 'uploads'
-  Dir.mkdir(upload_dir_name) unless Dir.exist?(upload_dir_name)
+  Dir.mkdir(@upload_dir_name) unless Dir.exist?(@upload_dir_name)
 
   filename = params[:file][:filename]
-  File.open(upload_dir_name + '/' + filename, "w") do |f|
+  File.open(@upload_dir_name + '/' + filename, "w") do |f|
     f.write(params[:file][:tempfile].read)
   end
-
-  return '/' + upload_dir_name + '/' + filename
+  return '/' + @upload_dir_name + '/' + filename
 end
 
 get '/' do
+  @files = Dir.entries(@upload_dir_name).reject { |f| f == '.' || f == '..' }
+
+  @files_ctime = @files.map do |f|
+    { name: f,
+    ctime: File.ctime(@upload_dir_name + '/' + f) }
+  end
   slim :index
 end
